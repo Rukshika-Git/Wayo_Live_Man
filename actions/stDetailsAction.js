af1640c78b6bf9f274b306e64c1f06ad07ab6b03
Rukshika Premathilaka <54591465+Rukshika-Git@users.noreply.github.com>
2022-09-15 19:07:37 +0530

    Add files via upload

---
+++ actions/stDetailsAction.js
@@ -0,0 +1,31 @@
+import Axios from "axios";
+
+//action creator
+export const loadStDetails = () => async (dispatch) => {
+  //fetch
+  if (localStorage.getItem("usValues") !== null) {
+    if (
+      Object.keys(JSON.parse(localStorage.getItem("usValues"))).length !== 0
+    ) {
+      const userLocalValues = JSON.parse(localStorage.getItem("usValues"));
+      const stDetails = await Axios.get(
+        `${process.env.REACT_APP_LMS_MAIN_URL}/auth/viewprofile/`,
+        {
+          headers: { Authorization: "Token " + userLocalValues.key },
+        }
+      );
+      dispatch({
+        type: "FETCH_STUDENT",
+        payload: stDetails.data,
+      });
+    } else {
+      dispatch({
+        type: "REMOVE_STUDENT",
+      });
+    }
+  } else {
+    dispatch({
+      type: "REMOVE_STUDENT",
+    });
+  }
+};
